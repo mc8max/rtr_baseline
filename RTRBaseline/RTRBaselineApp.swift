8812e4a1422d613c2a52f3866030e91715c021ce
Mc8Max <mc8max@gmail.com>
2026-02-19 22:44:40 +0700

    Initial Commit

---
+++ RTRBaseline/RTRBaselineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RTRBaselineApp.swift
+//  RTRBaseline
+//
+//  Created by Hoàng Trí Tâm on 19/2/26.
+//
+
+import SwiftUI
+
+@main
+struct RTRBaselineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
